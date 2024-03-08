e8136a729b36c6197e4f3e92a7763ab8ae544455
Suyash Sumaroo <suyash@codevigor.com>
2024-03-08 10:44:25 -0500

    webhook test commit

--- test.js
+++ test.js
@@ -1,16 +1,38 @@
-/**
- * Seed Function
- * (sails.config.bootstrap)
- *
- * A function that runs just before your Sails app gets lifted.
- * > Need more flexibility?  You can also create a hook.
- *
- * For more information on seeding your app with fake data, check out:
- * https://sailsjs.com/config/bootstrap
- */
+module.exports = {
+    friendlyName: "Read content of a file",
+    description: "Read content of a file",
+    inputs: {
+        file_path: {
+            type: "string",
+            example: "/project-folder/api/controllers/file-1.js",
+            required: true
+        }
+    },
 
-module.exports.bootstrap = async function() {
-    // if(sails.config.queue_settings.initialize_consumer_queue) {
-    //     sails.helpers.application.llm.initializeLlmQueue().then();
-    // }
+    fn: async function(inputs, exits) {
+        const fs = require("fs");
+
+        let filePath = inputs.file_path;
+
+        try {
+            fs.readFile(filePath, "utf8", async (err, fileContent) => {
+                if (err) {
+                    return exits.success({
+                        success: false
+                    });
+                } else {
+                    return exits.success({
+                        success: true,
+                        data: fileContent
+                    });
+                }
+            });
+        } catch (e) {
+            sails.log.debug(e);//debug
+
+            return exits.success({
+                success: false
+            });
+        }
+    }
 };

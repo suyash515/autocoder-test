7c5b451218aeb1f53bff7c869380d967f203aeed
suyash515 <suyash515@users.noreply.github.com>
2024-05-13 09:17:42 -0600

    Create the file /api/helpers/database/user/list-user.js

---
+++ api/helpers/database/user/list-user.js
@@ -0,0 +1,51 @@
+module.exports = {
+    friendlyName: "List User Helper",
+    description: "Helper to fetch user data from User model",
+    inputs: {
+        search_criteria: {
+            type: "ref",
+            required: true
+        },
+        sort_criteria: {
+            type: "ref",
+            required: false
+        },
+        limit: {
+            type: "number",
+            required: false
+        },
+        skip: {
+            type: "number",
+            required: false
+        },
+        populate: {
+            type: "ref",
+            required: false
+        }
+    },
+
+    fn: async function(inputs, exits) {
+        let promise = User.find(inputs.search_criteria),
+            recordList = [];
+
+        if (inputs.sort_criteria) {
+            promise.sort(inputs.sort_criteria);
+        }
+
+        if (inputs.limit) {
+            promise.limit(inputs.limit);
+        }
+
+        if (inputs.skip) {
+            promise.skip(inputs.skip);
+        }
+
+        if (inputs.populate) {
+            // No models to populate for User
+        }
+
+        recordList = await promise.then();
+
+        return exits.success(recordList);
+    }
+};

07e496783143b94019878529a8ccbbb3fa166bef
suyash515 <suyash515@users.noreply.github.com>
2024-05-13 09:14:19 -0600

    Create the file /api/models/User.js

---
+++ api/models/User.js
@@ -0,0 +1,23 @@
+/*
+ * User.js
+ * User Model
+ */
+
+module.exports = {
+    attributes: {
+        email: {
+            type: 'string',
+            required: true,
+            unique: true,
+            isEmail: true
+        },
+        name: {
+            type: 'string',
+            required: true
+        },
+        password: {
+            type: 'string',
+            required: true
+        }
+    }
+};

4a59e66532a05ac87d4c3f187fc3f3ad5fa4ae05
suyash515 <suyash515@users.noreply.github.com>
2024-05-13 14:50:48 -0600

    Create the file /api/helpers/database/user/update-user.js

---
+++ api/helpers/database/user/update-user.js
@@ -0,0 +1,20 @@
+module.exports = {
+    friendlyName: "Update user",
+    description: "Helper to update user data",
+    inputs: {
+        search_criteria: {
+            type: "json",
+            required: true
+        },
+        update_params: {
+            type: "json",
+            required: true
+        }
+    },
+
+    fn: async function(inputs, exits) {
+        await User.update(inputs.search_criteria, inputs.update_params);
+
+        return exits.success();
+    }
+};

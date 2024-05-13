3a417bf1dd11366b8a6b20dfe4d74ad9e5c264be
suyash515 <suyash515@users.noreply.github.com>
2024-05-13 14:50:37 -0600

    Create the file /api/controllers/User/update-user.js

---
+++ api/controllers/User/update-user.js
@@ -0,0 +1,57 @@
+module.exports = {
+    friendlyName: "Update user",
+    description: "Controller to update user",
+    inputs: {
+        data: {
+            type: {},
+            example: {
+                search_criteria: {
+                    id: "123456789"
+                },
+                update_params: {
+                    email: "value_here",
+                    name: "another_value_here", 
+                    password: "password_value_here"
+                }
+            }
+        },
+        auth: {
+            type: {},
+            example: {
+                app_token: ""
+            }
+        }
+    },
+    exits: {
+        jsonError: {
+            responseType: "jsonError"
+        },
+        success: {
+            responseType: "jsonOk"
+        }
+    },
+    fn: async function(inputs, exits) {
+        let error = [],
+            updateResponse = null;
+
+        try {
+            if (inputs.data && inputs.data.search_criteria && inputs.data.update_params) {
+                updateResponse = await sails.helpers.database.user.updateUser(inputs.data.search_criteria, inputs.data.update_params);
+
+                return exits.success({
+                    data: updateResponse
+                });
+            } else {
+                error.push(await sails.helpers.utility.error.getAppError("general.invalid_parameters"));
+
+                return exits.jsonError(error);
+            }
+        } catch (e) {
+            sails.log.debug("update-user.js : e"); //debug
+            sails.log.debug(e); //debug
+            error.push(await sails.helpers.utility.error.getAppError("general.unknown_error"));
+
+            return exits.jsonError(error);
+        }
+    }
+};
